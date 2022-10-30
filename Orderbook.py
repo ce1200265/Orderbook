74fc33aae8d0fd581315d4501ec52ec0ea7c8ec1
ce1200265 <117029137+ce1200265@users.noreply.github.com>
2022-10-30 21:16:16 +0530

    First Commit

---
+++ Orderbook.py
@@ -0,0 +1,12 @@
+
+
+class orderbook:
+
+    def addOrder(self,order):
+        if(self.book==None):
+            
+            new_book.add(order)
+
+            self.book = new_book
+        
+    
